3479002c9e605b5ddfe85a7b0d47c5112c13a51e
parth-verma7 <lcb2021028@iiitl.ac.in>
2023-08-26 18:21:12 +0530

    Protfolio Code

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd 
 import numpy as np
 import time
+import requests
 from PIL import Image
+from io import BytesIO
+
+# st.set_page_config(layout="wide")
 
 st.balloons()
 st.snow()
@@ -12,9 +16,11 @@ st.snow()
 
 col1, col2 = st.columns(2)
 with col1:
-    # image = Image.open('mirror-selfie-jawline.jpg')
-    url="https://github.com/parth-verma7/Portfolio/blob/main/mirror-selfie-jawline.jpg?raw=true"
-    st.image(url, caption='PARTH VERMA', width=250)
+    url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0bq64Eg5v0spKqJS6nUoUNjD_l02PINnjaVEVgJdSlReN_gCQwKkpk_PONzmJ8EvOEog&usqp=CAU"
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((250, 320))
+    st.image(img, caption='PARTH VERMA')
     
 with col2:
     st.title('Parth Verma')
@@ -24,66 +30,30 @@ with col2:
     st.markdown("[Github](https://github.com/parth-verma7) - Let's contribute")
     st.markdown("[Twitter](https://twitter.com/v_parth7) - Let's Explore")
 
+# st.header("<h1 style='text-align: center;'>Projects Section</h1>")
+st.markdown("<h3 style='text-align: center;'>Projects Section</h3>", unsafe_allow_html=True)
+st.header("")
 
+col1, col2 = st.columns(2)
+with col1:
+    image_url="https://miro.medium.com/v2/resize:fit:1400/1*4hCXr7Y6b8vDvQO31il38g.jpeg"
+    link_url="https://github.com/parth-verma7/DRONE"
+    st.markdown(f'<div style="text-align:center;"><a href="{link_url}" target="_blank"><img src="{image_url}" width="{330}" height="{250}">DRONE WCARL Project</a>', unsafe_allow_html=True)
+    
+with col2:
+    image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCTUwntPRB_U_l-FGJIYuM0aCdrPy_OvQ6D2AFl5rWFYaSIiKYsw9VqHmZowFuw6UMSpY&usqp=CAU"
+    link_url="https://github.com/parth-verma7/MovieRecommendation"
+    st.markdown(f'<div style="text-align:center;"><a href="{link_url}" target="_blank"><img src="{image_url}" width="{330}" height="{250}">Hybrid Moview Recommendation System</a>', unsafe_allow_html=True)
 
-# "Hello, world!"
-# # st.write("Hello, world!")
-# df=pd.DataFrame({'name':['Parth', 'Verma'], 'Number':[7,9]})
-# st.write(df)
-# # st.table(df)
-# st.dataframe(df)
+st.header("")
 
-# dataframe = np.random.randn(10, 20)
-# st.dataframe(dataframe)
-
-# chart_data = pd.DataFrame(
-#      np.random.randn(20, 3),
-#      columns=['a', 'b', 'c'])
-# st.line_chart(chart_data)
-
-# map_data = pd.DataFrame(
-#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-#     columns=['lat', 'lon'])
-# st.map(map_data)
-
-# x = st.slider('x')
-# st.write(x, 'squared is', x * x)
-
-# if st.checkbox('Show dataframe'):
-#     chart_data = pd.DataFrame(
-#        np.random.randn(20, 3),
-#        columns=['a', 'b', 'c'])
-#     # st.dataframe(chart_data)
-#     chart_data
-
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40]
-#     })
-# option = st.selectbox(
-#     'Which number do you like best?',
-#      df['first column'])
-# 'You selected: ', option
-
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
-
-# left_column, right_column = st.columns(2)
-# left_column.button('Press me!')
-# with right_column:
-#     chosen = st.radio(
-#         'Sorting hat',
-#         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-#     st.write(f"You are in {chosen} house!")
-
-# latest_iteration = st.empty()
-# bar = st.progress(0)
-
-# for i in range(100):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(2)
-
+col1, col2 = st.columns(2)
+with col1:
+    image_url="https://i.ytimg.com/vi/zACPSbUennk/maxresdefault.jpg"
+    link_url="https://github.com/parth-verma7/The-Yoga-Instruct"
+    st.markdown(f'<div style="text-align:center;"><a href="{link_url}" target="_blank"><img src="{image_url}" width="{330}" height="{250}">The YOGA Instruct</a>', unsafe_allow_html=True)
+    
+with col2:
+    image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdpG05KwKrZxem69amzWYoWCDJ3r5xMDFtqw&usqp=CAU"
+    link_url="https://github.com/parth-verma7/GestureDetection"
+    st.markdown(f'<div style="text-align:center;"><a href="{link_url}" target="_blank"><img src="{image_url}" width="{330}" height="{250}">Gesture Detection Recognition</a>', unsafe_allow_html=True)
